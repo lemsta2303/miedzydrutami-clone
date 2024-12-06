b5550324e8877d1bb5171e4058cfe57b86dce8a0
Stasiu <lemsta2303@gmail.com>
2024-12-06 19:41:56 +0100

    home almost done

--- prestashop/themes/child_classic/assets/js/script.js
+++ prestashop/themes/child_classic/assets/js/script.js
@@ -7,4 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
             searchWidget.classList.toggle('active');
         });
     }
+
+    const tabs = document.querySelectorAll('.tabs-list li');
+    const sections = document.querySelectorAll('.category-section');
+
+    if (tabs.length > 0 && sections.length > 0) {
+        tabs[0].classList.add('active'); // Pierwszy tab
+        sections[0].style.display = 'flex';
+    }
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            const target = tab.getAttribute('data-tab');
+
+            tabs.forEach(t => t.classList.remove('active'));
+            sections.forEach(section => {
+                section.style.display = 'none';
+            });
+
+            document.getElementById(target).style.display = 'flex';
+            tab.classList.add('active');
+        });
+    });
 });
